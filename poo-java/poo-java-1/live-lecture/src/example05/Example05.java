b4582c0e154d8fdb78c3bbee7177dff0d857db6c
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-04 18:56:24 -0300

    feat: live-lecture/example05

---
+++ poo-java/poo-java-1/live-lecture/src/example05/Example05.java
@@ -0,0 +1,20 @@
+package example05;
+
+public class Example05 {
+    public static void main(String[] args) {
+        int[][] matrix = new int[5][5];
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                matrix[i][j] = i + j;
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
