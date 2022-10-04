11876962033816c9865fccdc25c7712237c82882
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-04 18:56:13 -0300

    feat: live-lecture/example03

---
+++ poo-java/poo-java-1/live-lecture/src/example03/Example03.java
@@ -0,0 +1,18 @@
+package example03;
+
+public class Example03 {
+    public static void main(String[] args) {
+        int count = 1;
+
+        while (count <= 10) {
+            System.out.print(count + " ");
+            count++;
+        }
+
+        System.out.println();
+
+        for (count = 1; count <= 10; count++) {
+            System.out.print(count + " ");
+        }
+    }
+}
