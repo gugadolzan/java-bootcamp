77695279a9c6fcb2d6d6ee03089d48acda2cfc15
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-04 21:56:59 -0300

    feat(exercise-2): Exercise01 - ExceptionsExercise

---
+++ poo-java/poo-java-2/exercise-2/src/Exercise01/ExceptionsExercise.java
@@ -0,0 +1,33 @@
+package Exercise01;
+
+public class ExceptionsExercise {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 300;
+
+        // calculate1(a, b);
+        calculate2(a, b);
+    }
+
+    public static void calculate1(int a, int b) {
+        try {
+            int c = b / a;
+            System.out.println(c);
+        } catch (ArithmeticException e) {
+            System.out.println("An error occurred");
+        } finally {
+            System.out.println("Program finished");
+        }
+    }
+
+    public static void calculate2(int a, int b) throws IllegalArgumentException {
+        try {
+            int c = b / a;
+            System.out.println(c);
+        } catch (ArithmeticException e) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        } finally {
+            System.out.println("Program finished");
+        }
+    }
+}
