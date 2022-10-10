3a52ae85680c00b4ea8b6a2aa400201bf3354614
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-10 17:58:23 -0300

    feat(poo-java-4): models

---
+++ poo-java/poo-java-4/src/model/Bill.java
@@ -0,0 +1,56 @@
+package model;
+
+import java.util.List;
+
+public class Bill {
+    private Long code;
+    private Customer customer;
+    private List<Item> items;
+
+    public Bill(Long code, Customer customer, List<Item> items) {
+        this.code = code;
+        this.customer = customer;
+        this.items = items;
+    }
+
+    public Long getCode() {
+        return code;
+    }
+
+    public void setCode(Long code) {
+        this.code = code;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public double getTotal() {
+        double total = 0;
+        for (Item item : items) {
+            total += item.getPrice() * item.getQuantity();
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return "Bill{" +
+                "code=" + code +
+                ", customer=" + customer.getName() +
+                ", total=" + getTotal() +
+                '}';
+    }
+}
---
+++ poo-java/poo-java-4/src/model/Item.java
@@ -0,0 +1,57 @@
+package model;
+
+public class Item {
+    private Long code;
+    private String name;
+    private int quantity;
+    private double price;
+
+    public Item(Long code, String name, int quantity, double price) {
+        this.code = code;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Long getCode() {
+        return code;
+    }
+
+    public void setCode(Long code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "code=" + code +
+                ", name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                '}';
+    }
+}
