22820c95964e28679008a365cc42d1267cf6ea79
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-10 21:06:57 -0300

    feat(live-lecture): lambda

---
+++ poo-java/poo-java-4/live-lecture/src/lambda/Example02.java
@@ -0,0 +1,19 @@
+package lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Example02 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        numbers.forEach(n -> System.out.print(n + " - "));
+        System.out.println();
+
+        for (int i = 0; i < numbers.size(); i++) System.out.print(numbers.get(i) + " - ");
+        System.out.println();
+
+        for (Integer n : numbers) System.out.print(n + " - ");
+        System.out.println();
+    }
+}
---
+++ poo-java/poo-java-4/live-lecture/src/lambda/MainLambda.java
@@ -0,0 +1,16 @@
+package lambda;
+
+public class MainLambda {
+    interface InterfaceString {
+        String execute(String s);
+    }
+
+    public static void main(String[] args) {
+        InterfaceString interfaceString = s -> s + " World";
+        print("Hello", interfaceString);
+    }
+
+    public static void print(String text, InterfaceString interfaceString) {
+        System.out.println(interfaceString.execute(text));
+    }
+}
