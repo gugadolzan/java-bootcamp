ba50be2eb4cdb5c5a546b8cec789330db59b4464
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-11 11:57:44 -0300

    feat(poo-java-5/live-lecture): PrintOnPrinter util

---
+++ poo-java/poo-java-5/live-lecture/src/util/PrintOnPrinter.java
@@ -0,0 +1,9 @@
+package util;
+
+public class PrintOnPrinter implements Print {
+    @Override
+    public void print(String data) {
+        System.out.println("Printing to printer...");
+        System.out.println(data);
+    }
+}
--- poo-java/poo-java-5/live-lecture/src/util/PrintOnScreen.java
+++ poo-java/poo-java-5/live-lecture/src/util/PrintOnScreen.java
@@ -1,6 +1,6 @@
 package util;
 
-public class PrintScreen implements Print {
+public class PrintOnScreen implements Print {
     @Override
     public void print(String data) {
         System.out.println("Printing to screen...");
