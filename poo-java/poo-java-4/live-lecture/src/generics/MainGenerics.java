a89d7c515adfefa7693d3bdfaf0fe695cbea9ac1
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-10 21:07:10 -0300

    feat(live-lecture): generics

---
+++ poo-java/poo-java-4/live-lecture/src/generics/MainGenerics.java
@@ -0,0 +1,17 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainGenerics {
+    public static void main(String[] args) {
+        MyClass<Integer> obj1 = new MyClass<>(222);
+        MyClass<String> obj2 = new MyClass<>("Text");
+
+        System.out.println(obj1.getObj());
+        System.out.println(obj2.getObj());
+
+        // List of unknown type, not recommended
+        List<?> list = new ArrayList<>();
+    }
+}
