4e826884e67e7f774c1e7d48b07f7b8aed7a1f35
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-11 00:50:21 -0300

    feat(poo-java-5/live-lecture): SavingsAccountt

---
+++ poo-java/poo-java-5/live-lecture/src/model/SavingsAccount.java
@@ -0,0 +1,36 @@
+package model;
+
+import exception.InvalidNumberException;
+
+public class SavingsAccount extends Account {
+    private static double fee;
+
+    public SavingsAccount(int number, Client client) {
+        super(number, client);
+    }
+
+    public static double getFee() {
+        return fee;
+    }
+
+    public static void setFee(double fee) {
+        SavingsAccount.fee = fee;
+    }
+
+    private boolean hasEnoughBalance(double value) {
+        return getBalance() >= value + fee;
+    }
+
+    @Override
+    public boolean withdraw(double value) throws InvalidNumberException {
+        if (!hasEnoughBalance(value)) {
+            return false;
+        }
+        return super.withdraw(value + fee);
+    }
+
+    @Override
+    public String toString() {
+        return "Savings Account: " + super.toString();
+    }
+}
