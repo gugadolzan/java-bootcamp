365b3cc5fd91748938d242df7ac74440965df900
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-11 00:49:38 -0300

    feat(poo-java-5/live-lecture): CurrentAccount

---
+++ poo-java/poo-java-5/live-lecture/src/model/CurrentAccount.java
@@ -0,0 +1,26 @@
+package model;
+
+import exception.InvalidNumberException;
+
+public class CurrentAccount extends Account {
+    public CurrentAccount(int number, Client client) {
+        super(number, client);
+    }
+
+    private boolean hasEnoughBalance(double value) {
+        return getBalance() >= value;
+    }
+
+    @Override
+    public boolean withdraw(double value) throws InvalidNumberException {
+        if (!hasEnoughBalance(value)) {
+            return false;
+        }
+        return super.withdraw(value);
+    }
+
+    @Override
+    public String toString() {
+        return "Current Account: " + super.toString();
+    }
+}
