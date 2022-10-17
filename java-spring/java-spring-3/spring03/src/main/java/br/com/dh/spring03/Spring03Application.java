9066e7f5e038b8a5ba827dd8461d6b3ad83daa4d
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-17 04:21:13 -0300

    feat(spring03): Log4j2 usage

--- java-spring/java-spring-3/spring03/src/main/java/br/com/dh/spring03/Spring03Application.java
+++ java-spring/java-spring-3/spring03/src/main/java/br/com/dh/spring03/Spring03Application.java
@@ -1,12 +1,17 @@
 package br.com.dh.spring03;
 
+//import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@Log4j2
 @SpringBootApplication
 public class Spring03Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Spring03Application.class, args);
+        //        log.info("info");
+        //        log.warn("warn");
+        //        log.error("error");
     }
 }
