d41f75dfacb7c2859bbd720baebccfb642545122
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-17 01:20:26 -0300

    feat(spring03): NotFoundException

---
+++ java-spring/java-spring-3/spring03/src/main/java/br/com/dh/spring03/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.dh.spring03.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+}
