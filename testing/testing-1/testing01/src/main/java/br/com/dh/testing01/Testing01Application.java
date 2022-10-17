69e619ca28b504bfd3b5c58e31613ff90bf1c175
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-17 07:08:11 -0300

    feat(testing): testing01

---
+++ testing/testing-1/testing01/src/main/java/br/com/dh/testing01/Testing01Application.java
@@ -0,0 +1,12 @@
+package br.com.dh.testing01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testing01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testing01Application.class, args);
+    }
+}
---
+++ testing/testing-1/testing01/src/test/java/br/com/dh/testing01/Testing01ApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.dh.testing01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testing01ApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
