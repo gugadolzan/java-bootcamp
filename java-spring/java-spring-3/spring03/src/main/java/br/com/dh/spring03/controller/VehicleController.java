6cfe974ff0c077b56c1e794fa451ccec0db7f58d
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-17 01:21:19 -0300

    feat(spring03): VehicleController
    getAllVehicles and getVehicle methods.

---
+++ java-spring/java-spring-3/spring03/src/main/java/br/com/dh/spring03/controller/VehicleController.java
@@ -0,0 +1,33 @@
+package br.com.dh.spring03.controller;
+
+import br.com.dh.spring03.model.Vehicle;
+import br.com.dh.spring03.service.IVehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/vehicles")
+public class VehicleController {
+
+    @Autowired // Dependency Injection
+    private IVehicle service;
+
+    @GetMapping
+    public ResponseEntity<List<Vehicle>> getAllVehicles() {
+        return new ResponseEntity<>(service.getAllVehicles(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{plate}")
+    public ResponseEntity<Vehicle> getVehicle(@PathVariable String plate) {
+        Vehicle vehicle = service.getVehicle(plate);
+        return new ResponseEntity<>(vehicle, HttpStatus.OK);
+    }
+
+}
