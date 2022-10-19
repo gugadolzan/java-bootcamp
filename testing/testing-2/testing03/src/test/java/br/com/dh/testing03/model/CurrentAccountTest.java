43e01d3ec9787933e7f0cba521b77ff00ac78e61
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-19 20:40:33 -0300

    test(testing03): ParameterizedTest

--- testing/testing-2/testing03/src/test/java/br/com/dh/testing03/model/CurrentAccountTest.java
+++ testing/testing-2/testing03/src/test/java/br/com/dh/testing03/model/CurrentAccountTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import br.com.dh.testing03.exception.InvalidNumberException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CurrentAccountTest {
 
@@ -19,10 +21,23 @@ class CurrentAccountTest {
     void withdraw_returnsTrue_whenBalanceIsEnough()
         throws InvalidNumberException {
         double depositValue = 100;
-        double withdrawValue = 50;
+        double withdrawValue = depositValue / 2;
 
         currentAccount.deposit(depositValue);
-        currentAccount.withdraw(50);
+        currentAccount.withdraw(withdrawValue);
+
+        assertThat(currentAccount.getBalance())
+            .isEqualTo(depositValue - withdrawValue);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "100, 50", "50, 50", "10, 10" })
+    void withdraw_returnsTrue_whenBalanceIsEnough(
+        double depositValue,
+        double withdrawValue
+    ) throws InvalidNumberException {
+        currentAccount.deposit(depositValue);
+        currentAccount.withdraw(withdrawValue);
 
         assertThat(currentAccount.getBalance())
             .isEqualTo(depositValue - withdrawValue);
