a6093637a69baac5f3111a18e1784299922a35b5
gugadolzan <dolzan.gustavo@gmail.com>
2022-10-24 20:26:41 -0300

    test(testing03): CurrentAccountControllerTest
    newCurrentAccountBody_returnsCurrentAccount_whenAccountIsCreated and
    deposit_returnsUpdatedCurrentAccount_whenDepositIsSuccessful.

--- testing/testing-2/testing03/src/test/java/br/com/dh/testing03/controller/CurrentAccountControllerTest.java
+++ testing/testing-2/testing03/src/test/java/br/com/dh/testing03/controller/CurrentAccountControllerTest.java
@@ -6,6 +6,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import br.com.dh.testing03.dto.AccountDTO;
 import br.com.dh.testing03.model.CurrentAccount;
 import br.com.dh.testing03.service.CurrentAccountService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -95,10 +96,73 @@ class CurrentAccountControllerTest {
     }
 
     @Test
-    void newCurrentAccount() {}
+    void newCurrentAccountBody_returnsCurrentAccount_whenAccountIsCreated()
+        throws Exception {
+        BDDMockito
+            .when(service.newCurrentAccount(anyString()))
+            .thenReturn(currentAccount);
+
+        ResultActions response = mockMvc.perform(
+            post("/current-accounts/new")
+                .content(
+                    objectMapper.writeValueAsString(
+                        new AccountDTO(currentAccount.getClient())
+                    )
+                )
+                .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        response
+            .andExpect(status().isCreated())
+            .andExpect(
+                jsonPath(
+                    "$.client",
+                    CoreMatchers.is(currentAccount.getClient())
+                )
+            )
+            .andExpect(
+                jsonPath(
+                    "$.balance",
+                    CoreMatchers.is(currentAccount.getBalance())
+                )
+            );
+    }
 
     @Test
-    void deposit() {}
+    void deposit_returnsUpdatedCurrentAccount_whenDepositIsSuccessful()
+        throws Exception {
+        double depositValue = 100;
+
+        BDDMockito
+            .when(service.getAccount(anyInt()))
+            .thenReturn(currentAccount);
+        BDDMockito
+            .doAnswer(invocation -> {
+                currentAccount.deposit(depositValue);
+                return null;
+            })
+            .when(service)
+            .deposit(currentAccount.getNumber(), depositValue);
+
+        ResultActions response = mockMvc.perform(
+            patch(
+                "/current-accounts/dep/{number}/{value}",
+                currentAccount.getNumber(),
+                depositValue
+            )
+                .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        response
+            .andExpect(status().isOk())
+            .andExpect(
+                jsonPath(
+                    "$.client",
+                    CoreMatchers.is(currentAccount.getClient())
+                )
+            )
+            .andExpect(jsonPath("$.balance", CoreMatchers.is(depositValue)));
+    }
 
     @Test
     void withdraw() {}
